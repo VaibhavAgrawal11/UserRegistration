2b075e31f0fdd083dbef3d1706f6d4794594db74
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-03 17:54:01 +0530

    MODIFIED: Feature to validate last name.

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,11 +1,22 @@
 #!/bin/bash -x
-printf "Feature to validate first name."
+printf "Feature to validate last name."
 echo "Enter your first name:"
 read firstName
 namePat="^[A-Z]?[a-z]{2}"
+#VALIDATE THE FIRST NAME
 if [[ $firstName =~ $namePat ]];
 then
 	echo yes
 else
 	echo no
+fi
+echo "Enter your last name:"
+read lastName
+namePat="^[A-Z]?[a-z]{2}"
+#VALIDATE THE FIRST NAME
+if [[ $lastName =~ $namePat ]];
+then
+   echo yes
+else
+   echo no
 fi
