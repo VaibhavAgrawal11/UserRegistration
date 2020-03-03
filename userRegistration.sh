706d2625c4aca841d18eacce770c6d388dc55452
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-03 18:50:41 +0530

    MODIFIED: Added feature to validate email

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,22 +1,31 @@
 #!/bin/bash -x
-printf "Feature to validate last name."
+printf "Feature to validate email."
+
+function validate()
+{
+	userCase=$1
+	testCase=$2
+	if [[ $firstName =~ $namePat ]];
+	then
+   	echo yes
+	else
+  		echo no
+	fi
+}
+
 echo "Enter your first name:"
 read firstName
 namePat="^[A-Z]?[a-z]{2}"
 #VALIDATE THE FIRST NAME
-if [[ $firstName =~ $namePat ]];
-then
-	echo yes
-else
-	echo no
-fi
+validate $firstname $namePat 
+
 echo "Enter your last name:"
 read lastName
 namePat="^[A-Z]?[a-z]{2}"
 #VALIDATE THE FIRST NAME
-if [[ $lastName =~ $namePat ]];
-then
-   echo yes
-else
-   echo no
-fi
+validate $lastname $namePat 
+
+echo "Enter your email address:"
+read email
+emailPat=^abc.[a-zA-Z0-9]*@bl.\co
+validate $email $emailPat 
