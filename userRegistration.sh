785b85dde2c4f09fc6d0711264444102b9b107b9
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-04 10:16:17 +0530

    MODIFIED: Added feature to validate mobile number.

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,31 +1,41 @@
 #!/bin/bash -x
-printf "Feature to validate email."
-
+printf "Feature to validate mobile number."
+#cCONSTANTS
+COUNTRYCODE=91
 function validate()
 {
-	userCase=$1
-	testCase=$2
-	if [[ $firstName =~ $namePat ]];
+	userCase="$1"
+	testCase="$2"
+	if [[ $userCase =~ $testCase ]]
 	then
    	echo yes
 	else
   		echo no
 	fi
 }
-
+#VALIDATE THE FIRST NAME
 echo "Enter your first name:"
 read firstName
-namePat="^[A-Z]?[a-z]{2}"
+namePat="^[A-Z][a-z]{2}"
+validate $firstName $namePat 
 #VALIDATE THE FIRST NAME
-validate $firstname $namePat 
-
 echo "Enter your last name:"
 read lastName
-namePat="^[A-Z]?[a-z]{2}"
-#VALIDATE THE FIRST NAME
-validate $lastname $namePat 
-
+namePat="^[A-Z][a-z]{2}"
+validate $lastName $namePat 
+#VALIDATE EMAIL ADDRESS
 echo "Enter your email address:"
 read email
 emailPat=^abc.[a-zA-Z0-9]*@bl.\co
 validate $email $emailPat 
+#VALIDATE MOBILE NUMBER
+echo "Enter your mobile number:"
+read mobileNumber
+mobilePat=^"$COUNTRYCODE""[ ][0-9]{10}$"
+if [[ $mobileNumber =~ $mobilePat ]]
+then
+	echo yes
+else
+	echo no
+fi
+
